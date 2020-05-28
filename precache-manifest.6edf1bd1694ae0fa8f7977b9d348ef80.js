5adc361fc9528d5c403a1b4b4e8b156aa24450b0
OlehPalchyk <oleh.palchyk@bonial.com>
2020-05-28 14:24:08 +0300

    initial commit

---
+++ precache-manifest.6edf1bd1694ae0fa8f7977b9d348ef80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af27805431c8efca0bdd95115b893056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0393a1920771a86b4f06",
+    "url": "/static/css/main.c1436fcf.chunk.css"
+  },
+  {
+    "revision": "cbf40c0d946933542ac3",
+    "url": "/static/js/2.4a755a64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a755a64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0393a1920771a86b4f06",
+    "url": "/static/js/main.3c58be2a.chunk.js"
+  },
+  {
+    "revision": "349fd6561a9bac5ec0d2",
+    "url": "/static/js/runtime-main.c61f0ced.js"
+  }
+]);
